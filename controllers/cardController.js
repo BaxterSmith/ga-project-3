9e8644ea427bcdeb0370695730bdaff6ff971f8d
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-03-22 16:27:05 -0400

    card controller structure

---
+++ controllers/cardController.js
@@ -0,0 +1,27 @@
+const Card = require('../models/Card.js');
+
+const cardController = {
+    index: (req, res) => {
+
+    },
+    new: (req, res) => {
+
+    },
+    create: (req, res) => {
+
+    },
+    show: (req, res) => {
+
+    },
+    edit: (req, res) => {
+
+    },
+    update: (req, res) => {
+
+    },
+    delete: (req, res) => {
+
+    }
+};
+
+module.exports = cardController;
