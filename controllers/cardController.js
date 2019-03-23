38aba2a778bc6d9b307ba378d3730e83559f4673
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-03-23 11:44:27 -0400

    setup routes in card controller

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -2,25 +2,38 @@ const Card = require('../models/Card.js');
 
 const cardController = {
     index: (req, res) => {
-
+        Card.find().then((cards) => {
+            res.render('index', {cards});
+        });
     },
     new: (req, res) => {
-
+        res.render('new');
     },
     create: (req, res) => {
-
+        Card.create(req.body).then(card => {
+            res.redirect('/');
+        });
     },
     show: (req, res) => {
-
+        let cardId = req.params.cardId;
+        Card.findById(cardId).then(card => {
+            res.render('album', {card, cardId})
+        });
     },
     edit: (req, res) => {
-
+        Card.findById(req.params.cardId).then(card => {
+            res.render('edit', {card});
+        });
     },
     update: (req, res) => {
-
+        Card.findByIdAndUpdate(req.params.cardId, req.body, {new: true}).then(() => {
+            res.redirect(`/${req.params.cardId}`);
+        });
     },
     delete: (req, res) => {
-
+        Card.findByIdAndDelete(req.params.cardId).then(() => {
+            res.redirect('/');
+        });
     }
 };
 
