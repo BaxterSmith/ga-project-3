87db7b5a8a73d658c46ca5210d8b26635a1c7827
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-03-26 11:44:23 -0400

    structure edit form

--- client/src/components/EditForm.js
+++ client/src/components/EditForm.js
@@ -4,7 +4,33 @@ class EditForm extends Component {
     render() {
         return (
             <div>
-                
+                <form action="/" method="POST">
+                   <div>
+                       <label for="skillLevel">Skill Level</label>
+                       <input type="text" name="skillLevel" id="skillLevel"/>
+                   </div>
+                   <div>
+                       <label for="category">Category</label>
+                       <input type="text" name="category" id="category"/>
+                   </div>
+                   <div>
+                       <label for="question">Question</label>
+                       <textarea rows="30" cols="10" name="question" id="question"></textarea>
+                   </div>
+                   <div>
+                       <label for="answer">Answer</label>
+                       <input type="text" name="answer" id="answer"/>
+                   </div>
+                   <div>
+                       <label for="name">Your Name</label>
+                       <input type="text" name="name" id="name"/>
+                   </div>
+                   <div>
+                       <label for="age">Your Age</label>
+                       <input type="number" name="age" id="age"/>
+                   </div>
+                   <input type="submit" value="Add Card"/>
+               </form>
             </div>
         );
     }
