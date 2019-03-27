5efd1ce243b787b357b1ca87af9725f57f7b24b2
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-03-27 09:55:37 -0400

    fixed handle change error

--- client/src/components/EditForm.js
+++ client/src/components/EditForm.js
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class EditForm extends Component {
+    state = {
+        skillLevel: '',
+        category: '',
+        question: '',
+        answer: '',
+        name: '',
+        age: ''
+    }
+    handleChange = (evt) => {
+        const clonedFormData = {...this.state.formData}
+        clonedFormData[evt.target.name] = evt.target.value
+        this.setState({formData: clonedFormData});
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+    }
     render() {
         return (
             <div>
-                <form action="/" method="POST">
+                <form action="/" method="POST" onSubmit={this.handleSubmit}>
                    <div>
                        <label for="skillLevel">Skill Level</label>
-                       <input type="text" name="skillLevel" id="skillLevel"/>
+                       <input type="text" name="skillLevel" id="skillLevel" onChange={this.handleChange}/>
                    </div>
                    <div>
                        <label for="category">Category</label>
-                       <input type="text" name="category" id="category"/>
+                       <input type="text" name="category" id="category" onChange={this.handleChange}/>
                    </div>
                    <div>
                        <label for="question">Question</label>
-                       <textarea rows="30" cols="10" name="question" id="question"></textarea>
+                       <textarea rows="30" cols="10" name="question" id="question" onChange={this.handleChange}></textarea>
                    </div>
                    <div>
                        <label for="answer">Answer</label>
-                       <input type="text" name="answer" id="answer"/>
+                       <input type="text" name="answer" id="answer" onChange={this.handleChange}/>
                    </div>
                    <div>
                        <label for="name">Your Name</label>
-                       <input type="text" name="name" id="name"/>
+                       <input type="text" name="name" id="name" onChange={this.handleChange}/>
                    </div>
                    <div>
                        <label for="age">Your Age</label>
-                       <input type="number" name="age" id="age"/>
+                       <input type="number" name="age" id="age" onChange={this.handleChange}/>
                    </div>
                    <input type="submit" value="Add Card"/>
                </form>
