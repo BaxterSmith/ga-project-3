69635bc3c420cfcdd4ddc0fceb31b6cada4d54f2
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-03-28 09:02:53 -0400

    show and create working

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -3,26 +3,27 @@ const Card = require('../models/Card.js');
 const cardController = {
     index: (req, res) => {
         Card.find().then((cards) => {
-            res.render('index', {cards});
+            res.json(cards)
         });
     },
     new: (req, res) => {
-        res.render('new');
+        console.log('new')
     },
     create: (req, res) => {
         Card.create(req.body).then(card => {
+            card.save()
             res.redirect('/');
         });
     },
     show: (req, res) => {
         let cardId = req.params.cardId;
         Card.findById(cardId).then(card => {
-            res.render('album', {card, cardId})
+            res.json(card)
         });
     },
     edit: (req, res) => {
         Card.findById(req.params.cardId).then(card => {
-            res.render('edit', {card});
+            res.json(card)
         });
     },
     update: (req, res) => {
