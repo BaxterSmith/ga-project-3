6cceb83f968cbc931c581d513f19933a4bc3763e
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-03-28 09:05:03 -0400

    show and create working

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 const cardController = require('./controllers/cardController.js');
+const routes = require('./routes/index.js');
 
 app.use(express.json());
-app.get('/', (req, res) => {
-    console.log('Hello World!');
-});
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+app.use('/', routes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
