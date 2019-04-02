f84e586b34667ea7868f88e6c18e34335f52d66c
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-04-01 22:20:12 -0400

    created build folder

---
+++ client/build/precache-manifest.aedebd0675282caf08e6c428b5f09164.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "794f85b832a184ad8051",
+    "url": "/static/css/main.822e5a5d.chunk.css"
+  },
+  {
+    "revision": "794f85b832a184ad8051",
+    "url": "/static/js/main.1d2ba895.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "55abf9e501f82779df2a",
+    "url": "/static/js/2.608327ac.chunk.js"
+  },
+  {
+    "revision": "4ad3cb71d2fa27bd43a2a6b631478be0",
+    "url": "/index.html"
+  }
+];
