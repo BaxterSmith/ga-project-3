372adec4758ae5deb3db3b347604e404c7af7286
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-04-01 22:21:07 -0400

    created build folder

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const methodOverride = require('method-override');
-const cardController = require('./controllers/cardController.js');
 const routes = require('./routes/index.js');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client/build'));
 app.use('/', routes);
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
